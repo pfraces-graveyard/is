a3df9f8765222766c5ef43012a4e370871b6af46
pau fraces <pfraces@gmail.com>
2013-11-09 16:45:14 +0100

    [¶efactor] minimized commonjs syntax

--- is.js
+++ is.js
@@ -1,27 +1,31 @@
-module.exports.nil = function (obj) {
+var is = {};
+
+is.nil = function (obj) {
   return obj == null;
 };
 
-module.exports.bool = function (obj) {
+is.bool = function (obj) {
   return typeof obj === 'boolean';
 };
 
-module.exports.number = function (obj) {
+is.number = function (obj) {
   return typeof obj === 'number';
 };
 
-module.exports.string = function (obj) {
+is.string = function (obj) {
   return typeof obj === 'string';
 };
 
-module.exports.object = function (obj) { 
+is.object = function (obj) { 
   return Object.prototype.toString.call(obj) === '[object Object]';
 };
 
-module.exports.array = function (obj) {
+is.array = function (obj) {
   return obj instanceof Array;
 };
 
-module.exports.func = function (obj) {
+is.func = function (obj) {
   return typeof obj === 'function';
 };
+
+module.exports = is;
